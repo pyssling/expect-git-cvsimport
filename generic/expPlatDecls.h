be70d43dc275d99179ac8de076e986d0e538d5d1
David Gravereaux <davygrvy@users.sourceforge.net>
2001-11-07 10:06:30 +0000

    Massive restructuring done to the header files. Closely emulates the core.

--- generic/expPlatDecls.h
+++ generic/expPlatDecls.h
@@ -1,14 +1,51 @@
-/*
+/* ----------------------------------------------------------------------------
  * expPlatDecls.h --
  *
  *	Declarations of platform specific Expect APIs.
  *
+ * ----------------------------------------------------------------------------
+ *
+ * Written by: Don Libes, libes@cme.nist.gov, NIST, 12/3/90
+ * 
+ * Design and implementation of this program was paid for by U.S. tax
+ * dollars.  Therefore it is public domain.  However, the author and NIST
+ * would appreciate credit if this program or parts of it are used.
+ * 
+ * Copyright (c) 1997 Mitel Corporation
+ *	work by Gordon Chaffee <chaffee@bmrc.berkeley.edu> for the WinNT port.
+ *
+ * Copyright (c) 2001 Telindustrie, LLC
+ *	work by David Gravereaux <davygrvy@pobox.com> for any Win32 OS.
+ *
+ * ----------------------------------------------------------------------------
+ * URLs:    http://expect.nist.gov/
+ *	    http://expect.sf.net/
+ *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
+ * ----------------------------------------------------------------------------
  * RCS: @(#) $Id$
+ * ----------------------------------------------------------------------------
  */
 
 #ifndef _EXPPLATDECLS
 #define _EXPPLATDECLS
 
+/*
+ *  Pull in the definition of TCHAR.  Hopefully the compile flags
+ *  of the core are matching against your project build for these
+ *  public functions.  BE AWARE.
+ */
+#ifdef __WIN32__
+#   ifndef _TCHAR_DEFINED
+#	include <tchar.h>
+#	ifndef _TCHAR_DEFINED
+	    /* Borland seems to forget to set this. */
+	    typedef _TCHAR TCHAR;
+#	    define _TCHAR_DEFINED
+#	endif
+#   endif
+#endif
+
+	    
 /* !BEGIN!: Do not edit below this line. */
 
 /*

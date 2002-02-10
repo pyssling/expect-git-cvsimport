b9c04b2da0e871cdd1250bc77f566f072d7c354a
David Gravereaux <davygrvy@users.sourceforge.net>
2002-02-10 06:40:57 +0000

    fixing filename case problem

--- win/expWinInit.c
+++ win/expWinInit.c
@@ -1,61 +1,44 @@
-/* ----------------------------------------------------------------------------
- * expWinInit.c --
- *
- *	Contains startup code needed for the Expect extension on Windows.
- *
- * ----------------------------------------------------------------------------
- *
- * Written by: Don Libes, libes@cme.nist.gov, NIST, 12/3/90
- * 
- * Design and implementation of this program was paid for by U.S. tax
- * dollars.  Therefore it is public domain.  However, the author and NIST
- * would appreciate credit if this program or parts of it are used.
- * 
- * Copyright (c) 1997 Mitel Corporation
- *	work by Gordon Chaffee <chaffee@bmrc.berkeley.edu> for the WinNT port.
- *
- * Copyright (c) 2001 Telindustrie, LLC
- *	work by David Gravereaux <davygrvy@pobox.com> for any Win32 OS.
- *
- * ----------------------------------------------------------------------------
- * URLs:    http://expect.nist.gov/
- *	    http://expect.sf.net/
- *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
- * ----------------------------------------------------------------------------
- * RCS: @(#) $Id$
- * ----------------------------------------------------------------------------
- */
-
 #include "expWinInt.h"
 
 static ExpWinProcs asciiProcs = {
     0,
-    (HANDLE (WINAPI *)(CONST TCHAR *, DWORD, DWORD, SECURITY_ATTRIBUTES *, 
+    (HANDLE (WINAPI *)(LPCTSTR, DWORD, DWORD, SECURITY_ATTRIBUTES *, 
 	    DWORD, DWORD, HANDLE)) CreateFileA,
-    (BOOL (WINAPI *)(CONST TCHAR *, TCHAR *, LPSECURITY_ATTRIBUTES, 
-	    LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, CONST TCHAR *, 
+    (BOOL (WINAPI *)(LPCTSTR, LPTSTR, LPSECURITY_ATTRIBUTES, 
+	    LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCTSTR, 
 	    LPSTARTUPINFO, LPPROCESS_INFORMATION)) CreateProcessA,
-    (DWORD (WINAPI *)(CONST TCHAR *)) GetFileAttributesA,
-    (DWORD (WINAPI *)(CONST TCHAR *, TCHAR *, DWORD)) GetShortPathNameA,
-    (DWORD (WINAPI *)(CONST TCHAR *, CONST TCHAR *, CONST TCHAR *, DWORD, 
-	    TCHAR *, TCHAR **)) SearchPathA
+    (DWORD (WINAPI *)(LPCTSTR)) GetFileAttributesA,
+    (DWORD (WINAPI *)(LPCTSTR, LPTSTR, DWORD)) GetShortPathNameA,
+    (DWORD (WINAPI *)(LPCTSTR, LPCTSTR, LPCTSTR, DWORD, 
+	    LPTSTR, LPTSTR *)) SearchPathA,
+    (VOID (WINAPI *)(LPCTSTR)) OutputDebugStringA,
+    (DWORD (WINAPI *)(HMODULE, LPTSTR, DWORD)) GetModuleFileNameA,
+    (BOOL (WINAPI *)(LPCTSTR, LPCTSTR)) SetEnvironmentVariableA,
+    (BOOL (WINAPI *)(LPCTSTR, LPTSTR, DWORD)) GetEnvironmentVariableA,
+    (LONG (WINAPI *)(HKEY, LPCTSTR, DWORD, DWORD, CONST BYTE *,
+	    DWORD)) RegSetValueExA
 };
 
 static ExpWinProcs unicodeProcs = {
     1,
-    (HANDLE (WINAPI *)(CONST TCHAR *, DWORD, DWORD, SECURITY_ATTRIBUTES *, 
+    (HANDLE (WINAPI *)(LPCTSTR, DWORD, DWORD, SECURITY_ATTRIBUTES *, 
 	    DWORD, DWORD, HANDLE)) CreateFileW,
-    (BOOL (WINAPI *)(CONST TCHAR *, TCHAR *, LPSECURITY_ATTRIBUTES, 
-	    LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, CONST TCHAR *, 
+    (BOOL (WINAPI *)(LPCTSTR, LPTSTR, LPSECURITY_ATTRIBUTES, 
+	    LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCTSTR, 
 	    LPSTARTUPINFO, LPPROCESS_INFORMATION)) CreateProcessW,
-    (DWORD (WINAPI *)(CONST TCHAR *)) GetFileAttributesW,
-    (DWORD (WINAPI *)(CONST TCHAR *, TCHAR *, DWORD)) GetShortPathNameW,
-    (DWORD (WINAPI *)(CONST TCHAR *, CONST TCHAR *, CONST TCHAR *, DWORD, 
-	    TCHAR *, TCHAR **)) SearchPathW
+    (DWORD (WINAPI *)(LPCTSTR)) GetFileAttributesW,
+    (DWORD (WINAPI *)(LPCTSTR, LPTSTR, DWORD)) GetShortPathNameW,
+    (DWORD (WINAPI *)(LPCTSTR, LPCTSTR, LPCTSTR, DWORD, 
+	    LPTSTR, LPTSTR *)) SearchPathW,
+    (VOID (WINAPI *)(LPCTSTR)) OutputDebugStringW,
+    (DWORD (WINAPI *)(HMODULE, LPTSTR, DWORD)) GetModuleFileNameW,
+    (BOOL (WINAPI *)(LPCTSTR, LPCTSTR)) SetEnvironmentVariableW,
+    (BOOL (WINAPI *)(LPCTSTR, LPTSTR, DWORD)) GetEnvironmentVariableW,
+    (LONG (WINAPI *)(HKEY, LPCTSTR, DWORD, DWORD, CONST BYTE *,
+	    DWORD)) RegSetValueExW
 };
 
 ExpWinProcs *expWinProcs = &asciiProcs;
-
 
 /*
  *----------------------------------------------------------------------
@@ -70,11 +53,9 @@ ExpWinProcs *expWinProcs = &asciiProcs;
  *----------------------------------------------------------------------
  */
 void
-ExpWinInit (void)
+ExpWinInit(void)
 {
     if (TclWinGetPlatformId() == VER_PLATFORM_WIN32_NT) {    
 	expWinProcs = &unicodeProcs;
-    } else {
-	expWinProcs = &asciiProcs;
     }
 }
